84d7c1e93e6abc73e2b24fe6d5fcdfeab7838330
Rafly2312 <raflywiko@gmail.com>
2023-04-17 23:22:05 +0700

    tugaswp

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <hi><?php echo $judul ?></hi>
+    <h4> Nama </h4>
+    <ul type="disc">
+        <li> Nama Depan : Rafly Cikatik </li>
+        <li> Nama Belakang : Arta Sadullah </li>
+</ul>
+<br>
+<h4> Alamat </h4>
+<ul type="none">
+    <li> Jalan Narada Ujung No.D442 Johar Baru </li>
+</ul>
+
+<h4> Tempat Lahir </h4>
+<ul type="none">
+    <li>Jakarta</li>
+</ul>
+
+<h4> Olahraga Favorit</h4>
+<ul type="square">
+    <li> Sepak Bola </li>
+    <li> Basket </li>
+</ul>
+</section>
