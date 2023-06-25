67f3f01902fdc76b03b13bbdabdcdb3a81b9b69d
Rafly2312 <raflywiko@gmail.com>
2023-06-25 23:29:38 +0700

    tugaswp

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,10 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"JAVA EKA SABDA ALAM
-
-        NIM : 17200391
-        Kelas : 17.6A.07
-        ASAL J A K A R T A";
+        echo"Nama saya Imam Nawawi
+            Saya tingga di daerah Ciputat
+            olah raga yang saya sukai adalah
+            Bulutangkis";
     }
 }
--- application/views/v_about.php
+++ application/views/v_about.php
@@ -1,21 +1,26 @@
 <section>
-    <h1><?php echo $judul ?></h1>
-    <h4>Nama</h4>
-    <ul type="disc">
-        <li>Java Eka Sabda Alam</li>
-    </ul>
-    <br>
-    <h4>Alamat</h4>
-    <ul type="none">
-        <li> Jalan Kramat Sentiong, Jakarta Pusat</li>
-    </ul>
-    <h4>Tempat/Tanggal Lahir</h4>
-    <ul type="none">
-        <li>Jakarta, 22 Januari 2001</li>
-    </ul>
-    <h4>Hobi</h4>
-    <ul type="square">
-        <li>Cari Duit</li>
-        <li>Do'a Minta Dikasih Kaya Raya</li>
-    </ul>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Imam</li>
+		<li>Nama Belakang : Nawawi</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+	</ul>
+	
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Cirebon</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Bulutangkis</li>
+		<li>Catur</li>
+	</ul>
+	
+
 </section>
